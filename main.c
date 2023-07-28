319b4d2040cf75c3e6e00446b2c2c1db01bc76cb
xaviaxalee <davideffanga02@gmail.com>
2023-07-28 07:13:28 -0700

    project by: David & Edet

--- main.c
+++ main.c
@@ -2,44 +2,44 @@
 
 /**
  * main - entry point
- * @argc: argument count
- * @argv: argument vector
+ * @ac: arg count
+ * @av: arg vector
  *
  * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    info_t info_arr[] = { INFO_INIT };
-    int file_desc = 2;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-    asm ("mov %1, %0\n\t"
-         "add $3, %0"
-         : "=r" (file_desc)
-         : "r" (file_desc));
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 
-    if (argc == 2)
-    {
-        file_desc = open(argv[1], O_RDONLY);
-        if (file_desc == -1)
-        {
-            if (errno == EACCES)
-                exit(126);
-            if (errno == ENOENT)
-            {
-                _eputs(argv[0]);
-                _eputs(": 0: Can't open ");
-                _eputs(argv[1]);
-                _eputchar('\n');
-                _eputchar(BUF_FLUSH);
-                exit(127);
-            }
-            return (EXIT_FAILURE);
-        }
-        info_arr->readfd = file_desc;
-    }
-    populate_env_list(info_arr);
-    read_history(info_arr);
-    hsh(info_arr, argv);
-    return (EXIT_SUCCESS);
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
 
